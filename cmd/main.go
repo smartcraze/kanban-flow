dabb9abc2050c379d961aad302de2af27e7d4898
suraj <dev.surajv@gmail.com>
2025-08-15 11:06:39 +0000

    db connection successfull

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/smartcraze/kanban-flow/internal/db"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, reading from environment")
+	}
+	database := db.ConnectDB()
+
+	log.Println("⚙️Connected to DB:", database)
+
+	if database == nil {
+		log.Fatal("Database connection failed, exiting application")
+	}
+
 	app := gin.Default()
-	app.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
-	})
 	app.Run(":8000")
 
 }
